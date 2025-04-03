274d4e8b75d4a3187e6eded0051b4412adc3999b
Will Fox <william.macdonald.fox@gmail.com>
2025-04-03 18:15:38 -0400

    feat: set up state for movies in app and passed data to MoviesContainer

--- src/App/App.js
+++ src/App/App.js
@@ -2,17 +2,20 @@ import './App.css';
 import searchIcon from '../icons/search.png';
 
 // Example imports (for later):
-// import { useState, useEffect } from 'react';
-// import moviePosters from '../data/movie_posters';
-// import movieDetails from '../data/movie_details';
-// import MoviesContainer from '../MoviesContainer/MoviesContainer';
+import { useState, useEffect } from 'react'; // "Hey React, track this data and re-render if it changes"
+import moviePosters from '../data/movie_posters';
+import movieDetails from '../data/movie_details';
+import MoviesContainer from '../MoviesContainer/MoviesContainer';
 
 function App() {
+  const [movies, setMovies] = useState(moviePosters); // hook, lets you update that stateful data, automatically re-renders this component when data changes
   return (
     <main className='App'>
       <header>
         <h1>rancid tomatillos</h1>
-      </header>
+      </header> 
+
+      <MoviesContainer movies={movies} /> {/* render this child component passing movies as a prop */}
     </main>
   );
 }
