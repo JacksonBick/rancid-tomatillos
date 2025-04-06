3193df576efbf410458d2a0e839c602a7f228e3a
Will Fox <william.macdonald.fox@gmail.com>
2025-04-06 14:56:06 -0400

    fix: add import homeIcon from assets in App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import searchIcon from '../icons/search.png';
+import homeIcon from '../icons/home.png';
 import { useState, useEffect } from 'react';
 import moviePosters from '../data/movie_posters';
 import movieDetails from '../data/movie_details';
