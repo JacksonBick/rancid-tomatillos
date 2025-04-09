e71b9edd4aed9ab80104587a4d1ca040b5878dfb
Will Fox <william.macdonald.fox@gmail.com>
2025-04-08 21:08:15 -0400

    test: add cypress test for patchVote

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -20,7 +20,7 @@ describe('Main Page', () => {
     cy.get('.movie-container').should('exist')
     cy.get('.movie-card').should('have.length', 4)
   })
-  it('displays a vote-banner at the bottom of each poster', () => {
+  it('contains movie posters with an img, vote banner and buttons', () => {
     cy.get('.movie-card').first().find('.vote-banner').should('exist')
     cy.get('.movie-card').first().find('img').should('exist')
     cy.get('.movie-card').first().find('.vote-count').contains('Votes:')
@@ -35,3 +35,38 @@ describe('Main Page', () => {
     cy.get('.movie-card').last().find('button').contains('▼')
   })
 })
+
+describe('Voting Functionality', () => {
+  beforeEach(() => {
+    // Intercept the GET request to return fixture data
+    cy.intercept("GET", "https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies", {
+      statusCode: 200,
+      fixture: "movie_posters"
+    }).as("getMovies");
+
+    // Intercept PATCH request to simulate a successful upvote
+    cy.intercept("PATCH", "https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies/155", {
+      statusCode: 200,
+      body: {
+        id: 155,
+        poster_path: "https://image.tmdb.org/t/p/original//qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+        title: "The Dark Knight",
+        vote_count: 32545 // new updated count
+      }
+    }).as("patchVote");
+
+    cy.visit("http://localhost:3000/");
+
+    cy.wait("@getMovies"); // make sure movies are loaded first
+  });
+
+  it("sends a PATCH request and updates vote count when upvoted", () => {
+    
+    cy.get('.movie-card').first().find('.vote-count').should('have.text', 'Votes: 32544');
+    cy.get('.movie-card').first().find('button').contains('▲').click();
+
+    cy.wait("@patchVote");
+
+    cy.get('.movie-card').first().find('.vote-count').should('have.text', 'Votes: 32545');
+  });
+});
