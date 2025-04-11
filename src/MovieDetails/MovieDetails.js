031fd0e31574a29f96e232ce71ed685016302c56
Will Fox <william.macdonald.fox@gmail.com>
2025-04-10 23:02:38 -0400

    feat: refactor MovieDetails: - Use useParams to dynamically fetch movie data based on URL - Add loading and error states for detail view - Remove prop drilling from App component - Add Link-based navigation back to main page

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,31 +1,55 @@
 import './MovieDetails.css';
 import homeIcon from '../icons/home.png';
+import { useParams, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 
-function MovieDetails({ movieDetails, onBackClick }) {
-  if (!movieDetails) {
-    return <div>Loading...</div>;
-  }
+function MovieDetails() {
+  
+  const [movie, setMovie] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
+  const { movieId } = useParams()
 
+  useEffect(() => {
+    fetch(`https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies/${movieId}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch movie details');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setMovie(data);
+        setLoading(false);
+      })
+      .catch(err => {
+        setError("Sorry, we couldn’t load this movie.")
+        setLoading(false);
+      });
+  }, [movieId])
+
+  if (loading) return <p className="loading-message">Loading movie details...</p>
+  if (error) return <p className="error-message">{error}</p>
+  if (!movie) return null
   
   return (
     <section className='MovieDetails'>
-      {/* Back button with image */}
-      <button onClick={onBackClick} className="back-button">
-        <img src={homeIcon} alt="Back to Movies" />
-      </button>
+    <Link to="/" className="back-button">
+      <img src={homeIcon} alt="Back to Movies" />
+    </Link>
       
       <div className="movie-details-container">
         <img 
-          src={`https://image.tmdb.org/t/p/w500${movieDetails.backdrop_path}`}
-          alt={movieDetails.title}
+          src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+          alt={movie.title}
           className="movie-backdrop"
         />
         <div className="movie-info">
-          <h2>{movieDetails.title}</h2>
-          <p><strong>Genres:</strong> {movieDetails.genre_ids.join(', ')}</p>
-          <p><strong>Overview:</strong> {movieDetails.overview}</p>
-          <p><strong>Release Date:</strong> {movieDetails.release_date}</p>
+          <h2>{movie.title}</h2>
+          <p><strong>Genres:</strong> {movie.genre_ids.join(', ')}</p>
+          <p><strong>Overview:</strong> {movie.overview}</p>
+          <p><strong>Release Date:</strong> {movie.release_date}</p>
         </div>
       </div>
     </section>
